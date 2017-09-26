7211c288f2ed48588e5fc8bcbb28bfc79c4e4df1
evenkata <eknath_venkataramani@mcafee.com>
2017-09-25 22:36:05 -0700

    Adding basic packaging components

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""Setup file"""
+from setuptools import setup
+
+setup(name='vt3',
+      version='0.1',
+      description='Asynchronous API access to Virustotal',
+      url='https://github.com/ek-nath/vt3.git',
+      author='Eknath Venkataramani',
+      author_email='eknath.iyer@gmail.com',
+      license='MIT',
+      packages=['vt3'])
